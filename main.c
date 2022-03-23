33f282a13c3a4ce2d47dfb0676e1ce1914812004
TheFabulousDeath <53657534+TheFabulousDeath@users.noreply.github.com>
2022-03-23 17:39:29 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,34 +11,20 @@ typedef enum {false, true} bool;
 
 void printInst(List l){iterate(l, printInstructionList);}
 
-void initializeProgram(_part* partsArr){
+void initializeProgram(Part* partsArr){
 	stack instructionStack = initStack();
-l
 	*partsArr = calloc(_MAXPARTS, sizeof(_ELEMENT)); //Creating the global
 	loadParts(partsArr);
 }
 
 void restartProgram();//In case of an error this should reinitialize the program to reach a usable state.
 
-char* getUserInput()
-{
-	printf("  >");
-	size_t bufferSize = 100;
-	char* buffer = calloc(bufferSize, sizeof(char));
-	if(buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-	getline(&buffer, &bufferSize, stdin);
-	return buffer;
-}
-
 int main(int argc, *char argv[])
 {	
-
+	initializeProgram();
 	printWelcomeMessage();
 	char* orgbuff = getUserInput();
+
 	_part partsHead = partsArr;
 	int partshift = partsLen(partsArr); //do I need to keep track of the length?
 
