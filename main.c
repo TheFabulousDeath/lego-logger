16d52eb61b6ba8173f6ad141ca54d2062e9643da
TheFabulousDeath <53657534+TheFabulousDeath@users.noreply.github.com>
2022-03-23 17:24:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -20,26 +20,31 @@ l
 
 void restartProgram();//In case of an error this should reinitialize the program to reach a usable state.
 
+char* getUserInput()
+{
+	printf("  >");
+	size_t bufferSize = 100;
+	char* buffer = calloc(bufferSize, sizeof(char));
+	if(buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+	getline(&buffer, &bufferSize, stdin);
+	return buffer;
+}
+
 int main(int argc, *char argv[])
 {	
 
 	printWelcomeMessage();
-
+	char* orgbuff = getUserInput();
 	_part partsHead = partsArr;
 	int partshift = partsLen(partsArr); //do I need to keep track of the length?
 
 	while(1)
 	{
-		printf("  >");
-		char* buffer = calloc(bufferSize, sizeof(char));
-		char** orgbuff = &buffer;
-		if(buffer == NULL)
-		{
-			perror("Unable to allocate buffer");
-			exit(1);
-		}
-		size_t bufferSize = 100;
-		getline(&buffer, &bufferSize, stdin);
+
 		instructionList = interpret(&buffer, instructionList);
 		workInstruction(instructionList, &partshift, &partsHead, partsArr);
 		lastTimestamp = time(NULL);
