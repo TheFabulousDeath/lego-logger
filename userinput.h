892dfe3c201a74ec254c09faef66202138d47db0
TheFabulousDeath <53657534+TheFabulousDeath@users.noreply.github.com>
2022-03-23 17:39:24 +0100

    Create userinput.h

---
+++ userinput.h
@@ -0,0 +1,25 @@
+/*
+ * userinput.h
+ *
+ *  Created on: 23.03.2022
+ *      Author: vince
+ */
+
+#ifndef USERINPUT_H_
+#define USERINPUT_H_
+
+char* getUserInput()
+{
+	printf("  >");
+	size_t bufferSize = 100;
+	char* buffer = calloc(bufferSize, sizeof(char));
+	if(buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+	getline(&buffer, &bufferSize, stdin);
+	return buffer;
+}
+
+#endif /* USERINPUT_H_ */
