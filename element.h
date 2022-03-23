b417289f40fe65693927bc0e5c75b0a4cc9b6a2d
TheFabulousDeath <vlblankenburg@gmail.com>
2022-03-23 18:04:42 +0100

    Renamed the struct

--- element.h
+++ element.h
@@ -1,30 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-
 #ifndef ELEMENT_H_
 #define ELEMENT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
 
 #define _MAXPARTS 2000
 #define _ERROREL NULL
 #define _ERRORAM 0
 #define _ERRORID -1
 
-typedef struct
+typedef struct partElement
 {
 	int ID;
 	int amount;
-}_ELEMENT, *_part;
+}*Part;
 
-_part mkerrorel(){return _ERROREL;}
+Part mkerrorel(){return _ERROREL;}
 
-_part mknul(){
-	_part new;
-	new = calloc(1, sizeof(_ELEMENT));
-	//check if calloc worked
+Part mknul(){
+	Part new = calloc(1, sizeof(struct partElement));
 	if(new != NULL) return new;
 	else {
-		fprintf(stderr, "Calloc failed, NULL returned");
 		return NULL;
 	}
 }
@@ -33,12 +29,12 @@ int mkerroram(){return _ERRORAM;}
 
 int mkerrorid(){return _ERRORID;}
 
-_part _remove(_part e){
+Part _remove(Part e){
 	free(e);
 	return _ERROREL;
 }
 
-_part edit(_part e, int _amount){
+Part edit(Part e, int _amount){
 	if(e->amount == _ERRORAM || e->ID == _ERRORID || e == _ERROREL){
 		fprintf(stderr, "Faulty element detected, entry removed");
 		return _remove(e);
@@ -47,9 +43,9 @@ _part edit(_part e, int _amount){
 	return e;
 }
 
-_part add(int id, int amount)
+Part add(int id, int amount)
 {
-	_part new = mknul();
+	Part new = mknul();
 	if(id == _ERRORID || amount == _ERRORAM){
 		return _remove(new);
 	}
