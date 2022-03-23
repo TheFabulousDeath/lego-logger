93eb8a20a1b4373893419501000c07bf2dd0913e
TheFabulousDeath <vlblankenburg@gmail.com>
2022-03-23 17:48:09 +0100

    Small changes from the todo

--- main.c
+++ main.c
@@ -13,45 +13,37 @@ void printInst(List l){iterate(l, printInstructionList);}
 
 void initializeProgram(_part* partsArr){
 	stack instructionStack = initStack();
-
+l
 	*partsArr = calloc(_MAXPARTS, sizeof(_ELEMENT)); //Creating the global
 	loadParts(partsArr);
 }
 
 void restartProgram();//In case of an error this should reinitialize the program to reach a usable state.
 
-
-
 int main(int argc, *char argv[])
 {	
 
 	printWelcomeMessage();
-	
-	char* buffer;
-	size_t bufferSize = 100; //getline stuff
-	
-	
+
 	_part partsHead = partsArr;
-	int partshift = partsLen(partsArr);
-	int lastTimestamp = time(NULL);
+	int partshift = partsLen(partsArr); //do I need to keep track of the length?
+
 	while(1)
 	{
-	printf("  >");
-	buffer = (char *) calloc(bufferSize, sizeof(char));
-	char** orgbuff = &buffer;
-	if(buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-	getline(&buffer, &bufferSize, stdin);
-	instructionList = interpret(&buffer, instructionList);
-	if(head(instructionList) && head(instructionList)->timestamp >= lastTimestamp)
-	{
-		instructionList = workInstruction(instructionList, &partshift, &partsHead, partsArr);
-	}
-	lastTimestamp = time(NULL);
-	free(orgbuff);
+		printf("  >");
+		char* buffer = calloc(bufferSize, sizeof(char));
+		char** orgbuff = &buffer;
+		if(buffer == NULL)
+		{
+			perror("Unable to allocate buffer");
+			exit(1);
+		}
+		size_t bufferSize = 100;
+		getline(&buffer, &bufferSize, stdin);
+		instructionList = interpret(&buffer, instructionList);
+		workInstruction(instructionList, &partshift, &partsHead, partsArr);
+		lastTimestamp = time(NULL);
+		free(orgbuff);
 	}
 	return 1;
 }
