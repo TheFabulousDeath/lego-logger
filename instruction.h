6afd1b7aaccaa5c7cc8a20d9a123dd2005d3f68c
TheFabulousDeath <53657534+TheFabulousDeath@users.noreply.github.com>
2022-03-27 12:20:36 +0200

    Update instruction.h

--- instruction.h
+++ instruction.h
@@ -1,61 +1,46 @@
 #ifndef INSTRUCTION_H_
 #define INSTRUCTION_H_
 
-#define INSTRUCTIONSIZE 8
-#define _ERRORINSTRUCTION -1
+#define _ERRORINSTRUCTIONNUMBER -1
 #define DEFAULT 0
 
-#include <time.h> //including timestamp
-#include <ctype.h> //uppercasing
 #include <stdlib.h>
 #include "element.h"
-#include "stack.h"
 
 typedef struct instructionElement{
 	int instrNum;
-	int partNum;
+	int partID;
 	int amount;
-	int timestamp;
 	struct instructionElement * nextp;
 }*instruction;
 
-_instRef initInstruction(){
-	_instRef new = malloc(sizeof(instructionElem));
-	new -> instrNum = _ERRORINSTRUCTION;
-	new -> partNum = _ERRORID;
+instruction initInstruction(){
+	instruction new = malloc(sizeof(struct instructionElement));
+	new -> instrNum = _ERRORINSTRUCTIONNUMBER;
+	new -> partID = _ERRORID;
 	new -> amount = _ERRORAM;
-	new -> timestamp = time(NULL);
-	new -> nextp = _ERROREL;
+	new -> nextp = _ERRORPART;
 	return new;
 }
 
-
-List cancelInstruction(List instructionList, int* partshift, _part* partHead, _part partsArr){
-	while(instructionList)
-	{
-		switch(head(instructionList)->instrNum){
-		case(ADD):
-			head(instructionList)->instrNum = REMOVE;
-			head(instructionList)->timestamp = time(NULL);
-			editPart(head(instructionList),partshift, partHead, partsArr);
-			return tail(instructionList);
-		case(REMOVE):
-			head(instructionList)->instrNum = ADD;
-			head(instructionList)->timestamp = time(NULL);
-			editPart(head(instructionList),partshift, partHead, partsArr);
-			return tail(instructionList);
-		default:
-			if(tail(instructionList)) {
-				//memory Leck
-				instructionList = tail(instructionList);
-			} else return NULL;
-		}
-	}
-	return NULL;
+void destroyInstruction(instruction instruction){
+	free(instruction);
+	instruction = NULL;
 }
 
-void printInstructionList(Value e)
-{
-	fprintf(stderr, "Instruction name: %s; Part Number: %d; Amount: %d\n", _instructions[e->instrNum], e->partNum, e->amount);
+void changeInstructionNumber(instruction instruction, int instructionNumber){
+	if(instructionNumber == _ERRORINSTRUCTIONNUMBER) return;
+	instruction->instrNum = instructionNumber;
 }
+
+void changePartID(instruction instruction, int partID){
+	if(partID == _ERRORID) return;
+	instruction->partID = partID;
+}
+
+void changePartAmount(instruction instruction, int partAmount){
+	if(partAmount == _ERRORAM) return;
+	instruction->amount = partAmount;
+}
+
 #endif /* INTERPRETER_H_ */
